66f0897a7548553311200951f925bfe2e1260641
stone <ccd100@ccc.com>
2025-02-26 00:36:24 +0800

    添加okx 交易所代币查询

---
+++ ca/exchange.py
@@ -0,0 +1,9 @@
+from ca.binance import get_binance_price
+from ca.okx import get_okx_price
+
+def get_exchange_price(symbol):
+    price = get_binance_price(symbol)
+    if price is not None and isinstance(price, int) and price == 0:
+        price = get_okx_price(symbol)
+    return price    
+    
