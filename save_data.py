6aae8210e4f238f984b664c84b6ae2a35ecc34d5
stone <ccd100@ccc.com>
2025-03-01 12:12:43 +0800

    添加微信信息

--- save_data.py
+++ save_data.py
@@ -3,12 +3,51 @@ from queue import Empty
 from ca.ca_info import is_solca, is_eths, math_price, math_cex_price, math_km, math_percent, math_bjtime, get_bundles, is_cexToken, is_pump
 
 import time
+import requests
+import json
+
 
 wcf = Wcf()
 groups = ["58224083481@chatroom"]
 wcf.enable_receiving_msg()
 
 print('机器人启动')
+
+def add_wx_info(roomid,wxId,wxNick,address,times):
+    url = f"http://localhost:8080/api/wxInfo/add"
+    headers = {
+    "Content-Type": "application/json"
+    }
+    data = {
+        "roomId": roomid,
+        "wxId": wxId,
+        "wxNick": wxNick,
+        "address": address,
+        "times":times
+    }
+    # 发送 GET 请求
+    response = requests.post(url, json=data,headers=headers)        
+    print(response);    
+
+def get_wx_info(roomid,ca):
+    url = f"http://localhost:8080/api/wxInfo/get?roomId={roomid}&address={ca}"
+    
+    # 发送 GET 请求
+    response = requests.get(url)
+
+    # 获取响应内容
+    value = response.text
+    print(f"返回数据：{value[:100]}...")  # 仅输出前100个字符，防止过长
+
+    # 如果返回为空，直接返回
+    if not value:
+        print("返回数据为空，返回 0")
+        return 0
+    
+    data = json.loads(value)  # 将响应内容转换为字典
+    dataList = data.get("data",[])
+    print(f"数据: {dataList}")
+    return dataList
 
 ca_datas = [] 
 while wcf.is_receiving_msg():
@@ -32,12 +71,16 @@ while wcf.is_receiving_msg():
                     
                     ca_data = [msg.roomid, caller_wxid, caller_simulate_name, ca, query_time]
                     ca_datas.append(ca_data) 
-                    print(ca_datas)      
-        
+                    print(ca_datas)   
+                    add_wx_info(roomid,caller_wxid,caller_simulate_name,ca,query_time)
+                    
+                    
         except Empty:
             continue
         except Exception as e:
             print(e)
 
 
+# add_wx_info("111","123","xx","ca",time.time()*1000)
+# get_wx_info("111","ca")
 wcf.keep_running()   
